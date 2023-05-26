e5a8bd3963f25c025e29613031506ca684ba669a
JorgeAGB <jorgegbellos@gmail.com>
2023-05-26 11:06:31 -0600

    Final structure of contact and footer components

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './contact.css'
+import emailjs from '@emailjs/browser'
 
 export const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_c4t8e7r', 'template_im1t54f', form.current, 'b--_rDJe68wi6Wjxd')
+
+    e.target.reset()
+  };
+
   return (
     <section id="contact">
-
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Full name" required />
+          <input type="email" name="email" placeholder='Email' required />
+          <textarea name="message" placeholder="Tell me about your idea!" rows="7" required ></textarea>
+          <button type='submit' className='btn btn-primary'>Contact me!</button>
+        </form>
+      </div>
     </section>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebook} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {FaTwitterSquare} from 'react-icons/fa'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="" className='footer__logo'>
+        Dev in dev
+      </a>
+      <ul className='permalinks'>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experiences">Experiences</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebook/></a>
+        <a href="https://instagram.com"><FaInstagram/></a>
+        <a href="https://twitter.com"><FaTwitterSquare/></a>
+      </div>
+      <div className="footer__copyrights">
+        <small>&copy; Devindev. All Rights Reserved</small>
+      </div>
+    </footer>
   )
 }
